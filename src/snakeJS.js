23d075a630c48399a19cf5d37db59f91090ec37e
Zee Abdo <mr.ziad.abdo@gmail.com>
2014-07-06 16:16:47 -0700

    *Spiked for a while to figure out how   to work out timing and making sure that   the snake had visible movement and a delay   between movement. *I think I have an understanding for  the next few steps that I need to consider.  I can either move to making the snake move  or generate conditions for death. *I think it makes more sense to consider how  i'm going to make the snake move first that  way I have a way of testing out the death  conditions. *also... I need to write tests. boo.

--- src/snakeJS.js
+++ src/snakeJS.js
@@ -8,21 +8,28 @@ window.addEventListener( 'load', function(){
 function SnakeGameController(){
 	this.View = new SnakeView();
 	this.Model = new SnakeModel();
+	this.conInterval = setInterval(); 
 }
 
 SnakeGameController.prototype = {
 	startGame: function(){
-		while(this.gameIsActive()){
-			this.View.refreshScreen();
-			this.View.updateSnakeScreen(this.Model.allSnakeBodyPositions());
-			this.Model.moveSnake();
-		}
+		this.conInterval = setInterval(this.executeTurn.bind(this), 500)
+	},
+
+	executeTurn: function(){
+		this.View.refreshScreen();
+		this.View.updateSnakeScreen(this.Model.allSnakeBodyPositions());
+		this.Model.moveSnake();
+		console.log(this.Model.snakeBody[1].boardPos)
+		this.gameIsActive()
 	},
 
 	gameIsActive: function(){
 		if(this.Model.snakeBody[1].boardPos< 20){
 			return true
 		}else{
+			clearInterval(this.conInterval)
+			console.log("GAME OVER")
 			return false
 		}
 	}
